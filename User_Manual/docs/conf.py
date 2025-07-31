542485c380a2b5b1484620334dd574c9872b1d84
Hyoungmin Woo <woo.hyoungmin@epa.gov>
2025-07-30 21:59:37 -0400

    Update conf.py
    fixed minor error

--- User_Manual/docs/conf.py
+++ User_Manual/docs/conf.py
@@ -266,7 +266,7 @@ texinfo_documents = [
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/3': None}
+intersphinx_mapping = {'https://docs.python.org/3', None}
 
 # -- Options for todo extension ----------------------------------------------
 
