821626d3c3ae882c64adbc16091ddd303fd648d4
Hyoungmin Woo <woo.hyoungmin@epa.gov>
2025-07-30 21:55:27 -0400

    Update conf.py
    updated sphinx configuration

--- User_Manual/docs/conf.py
+++ User_Manual/docs/conf.py
@@ -73,7 +73,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -266,7 +266,7 @@ texinfo_documents = [
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'https://docs.python.org/3': None}
 
 # -- Options for todo extension ----------------------------------------------
 
